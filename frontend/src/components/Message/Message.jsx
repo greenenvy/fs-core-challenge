82bfc1232d3d14bbf26d18b32fd1b9cf6aa61d49
Stefan Colic <stefan.colic4@gmail.com>
2024-01-25 11:50:17 +0100

    Moving the message function

--- frontend/src/components/Message/Message.jsx
+++ frontend/src/components/Message/Message.jsx
@@ -1,38 +1,38 @@
 import React from "react";
 import "./Message.css";
 
+const formatDate = (date) => {
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let day = date.getDate().toString().padStart(2, "0");
+  let month = monthNames[date.getMonth()];
+  let year = date.getFullYear();
+
+  let hours = date.getHours().toString().padStart(2, "0");
+  let minutes = date.getMinutes().toString().padStart(2, "0");
+
+  // Omit the time part if it's 00:00
+  if (hours === "00" && minutes === "00") {
+    return `${day} ${month} ${year}`;
+  } else {
+    return `${day} ${month} ${year} ${hours}:${minutes}`;
+  }
+};
+
 const Message = ({ message, loggedOnUser }) => {
-  const formatDate = (date) => {
-    const monthNames = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
-
-    let day = date.getDate().toString().padStart(2, "0");
-    let month = monthNames[date.getMonth()];
-    let year = date.getFullYear();
-
-    let hours = date.getHours().toString().padStart(2, "0");
-    let minutes = date.getMinutes().toString().padStart(2, "0");
-
-    // Omit the time part if it's 00:00
-    if (hours === "00" && minutes === "00") {
-      return `${day} ${month} ${year}`;
-    } else {
-      return `${day} ${month} ${year} ${hours}:${minutes}`;
-    }
-  };
-
   return (
     <div
       className={
