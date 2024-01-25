ef91ffa9fe73dc8305ca393d419f6a59788dd92d
Stefan Colic <stefan.colic4@gmail.com>
2024-01-25 11:26:43 +0100

    Getting all the message on entering a valid username

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Chat.css";
 import Message from "../Message/Message";
 import LoginScreen from "../LoginScreen/LoginScreen";
@@ -9,39 +9,7 @@ import Input from "../Input/Input";
 const Chat = () => {
   const [error, setError] = useState(null);
   const [loggedOnUser, setLoggedOnUser] = useState("");
-  const messages = [
-    {
-      sender: "joffrey",
-      content: "Brilliant",
-      posted: new Date(1520636400000),
-    },
-    {
-      sender: "NINJA",
-      content: "Great resource, thanks",
-      posted: new Date(1520672100000),
-    },
-    {
-      sender: "i am mister brilliant",
-      content: "THANKSSSS!!!!!",
-      posted: new Date(1520673000000),
-    },
-    {
-      sender: "martin57",
-      content: "Thanks Peter",
-      posted: new Date(1520673540000),
-    },
-    {
-      sender: "Patricia",
-      content: "Sounds good to me!",
-      posted: new Date(1520673720000),
-    },
-    {
-      sender: "user1",
-      content:
-        "Hey folks! I wanted to get in touch with you regarding the project. Please, let me know how you plan to contribute.",
-      posted: new Date(1520673720000),
-    },
-  ];
+  const [messages, setMessages] = useState([]);
 
   const login = (username) => {
     axios
@@ -58,6 +26,19 @@ const Chat = () => {
         }
       });
   };
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8888/message/all")
+      .then((response) => {
+        setMessages(response.data);
+      })
+      .catch((error) => {
+        if (error.response) {
+          setError(error.response.data);
+        }
+      });
+  }, [loggedOnUser]);
 
   return (
     <div className="chat">
