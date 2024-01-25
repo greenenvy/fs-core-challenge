887b4447721e40376b066e19df267116ec8bab5f
Stefan Colic <stefan.colic4@gmail.com>
2024-01-25 11:48:36 +0100

    Formating date

--- frontend/src/components/Message/Message.jsx
+++ frontend/src/components/Message/Message.jsx
@@ -2,6 +2,37 @@ import React from "react";
 import "./Message.css";
 
 const Message = ({ message, loggedOnUser }) => {
+  const formatDate = (date) => {
+    const monthNames = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ];
+
+    let day = date.getDate().toString().padStart(2, "0");
+    let month = monthNames[date.getMonth()];
+    let year = date.getFullYear();
+
+    let hours = date.getHours().toString().padStart(2, "0");
+    let minutes = date.getMinutes().toString().padStart(2, "0");
+
+    // Omit the time part if it's 00:00
+    if (hours === "00" && minutes === "00") {
+      return `${day} ${month} ${year}`;
+    } else {
+      return `${day} ${month} ${year} ${hours}:${minutes}`;
+    }
+  };
+
   return (
     <div
       className={
@@ -12,9 +43,7 @@ const Message = ({ message, loggedOnUser }) => {
         {loggedOnUser === message.sender ? "You" : message.sender}
       </div>
       <div className="content">{message.content}</div>
-      <div className="date">
-        {new Date(message.posted).toLocaleDateString()}
-      </div>
+      <div className="date">{formatDate(new Date(message.posted))}</div>
     </div>
   );
 };
