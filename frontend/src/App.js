f94179fbc20261a7110873900ade5846b20eb39e
Stefan Colic <stefan.colic4@gmail.com>
2024-01-25 10:16:21 +0100

    Adding Chat component with boundaries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Chat from "./components/Chat/Chat";
 
 function App() {
-  return <div className="App">Doodle Chat</div>;
+  return (
+    <div className="container">
+      <Chat></Chat>
+    </div>
+  );
 }
 
 export default App;
