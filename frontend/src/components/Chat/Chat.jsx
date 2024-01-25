20b16935b89796f3154f2df4a194906685e34c9a
Stefan Colic <stefan.colic4@gmail.com>
2024-01-25 10:24:54 +0100

    Changing sender in the static test data for both mongo and UI

--- frontend/src/components/Chat/Chat.jsx
+++ frontend/src/components/Chat/Chat.jsx
@@ -4,32 +4,32 @@ import "./Chat.css";
 const Chat = () => {
   const messages = [
     {
-      sender: "65b2104a7fbdbe6087ce1096",
+      sender: "joffrey",
       content: "Brilliant",
       posted: new Date(1520636400000),
     },
     {
-      sender: "65b2104a7fbdbe6087ce1097",
+      sender: "NINJA",
       content: "Great resource, thanks",
       posted: new Date(1520672100000),
     },
     {
-      sender: "65b2104a7fbdbe6087ce1098",
+      sender: "i am mister brilliant",
       content: "THANKSSSS!!!!!",
       posted: new Date(1520673000000),
     },
     {
-      sender: "65b2104a7fbdbe6087ce1099",
+      sender: "martin57",
       content: "Thanks Peter",
       posted: new Date(1520673540000),
     },
     {
-      sender: "65b2104a7fbdbe6087ce109a",
+      sender: "Patricia",
       content: "Sounds good to me!",
       posted: new Date(1520673720000),
     },
     {
-      sender: "65b2104a7fbdbe6087ce109b",
+      sender: "user1",
       content:
         "Hey folks! I wanted to get in touch with you regarding the project. Please, let me know how you plan to contribute.",
       posted: new Date(1520673720000),
